0288fa9db7c9eeb36204201022cfdce889c10552
arjoo <2009arjoo@gmail.com>
2024-09-10 17:14:08 +0530

    added changes

--- ollama/src/main/java/com/arjoo/ollama/ChatController.java
+++ ollama/src/main/java/com/arjoo/ollama/ChatController.java
@@ -2,10 +2,7 @@ package com.arjoo.ollama;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
